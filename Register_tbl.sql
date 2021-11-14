9ae76915bc910a01fc42a809a52c27b1bf5746c5
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2021-11-13 21:00:52 -0800

    Delete Register_tbl.sql

--- Register_tbl.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `supply_db`.`register_tbl` (
-  `Userid` INT NOT NULL AUTO_INCREMENT,
-  `FName` VARCHAR(45) NULL,
-  `LName` VARCHAR(45) NULL,
-  `Gender` VARCHAR(45) NULL,
-  `Email` VARCHAR(45) NULL,
-  `Password` VARCHAR(45) NULL,
-  PRIMARY KEY (`Userid`));
